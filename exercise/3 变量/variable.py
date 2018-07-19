9b68567fe20943c192727516d801047221b51041
wiilz <rayleignze@gmail.com>
2018-07-19 22:59:43 +0800

    complete chapter 3

---
+++ exercise/3 变量/variable.py
@@ -0,0 +1,5 @@
+dollar = 100
+exchange_rate = 6.77
+print(dollar,"美元可兑换人民币",dollar*exchange_rate,"元")
+print("{0}美元可兑换人民币{1}元".format(dollar,dollar*exchange_rate))
+print("{dol}美元可兑换人民币{yuan}元".format(dol = dollar,yuan = dollar * exchange_rate))
